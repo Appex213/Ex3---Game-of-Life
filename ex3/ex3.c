4d148cb6f961fa059e07cfc12e5554ecec1209fb
Matan <Matan@MATAN-PC>
2016-12-23 00:35:06 +0200

    More functions

--- ex3/ex3.c
+++ ex3/ex3.c
@@ -11,12 +11,17 @@
 #define CLR_PLYR 'R'
 
 
-void GameStartup(char board[MAX_WIDTH][MAX_HEIGHT], int* width, int* height);
+void GameStart(char board[MAX_WIDTH][MAX_HEIGHT], int* width, int* height);
 void InitializeBoard(char board[MAX_WIDTH][MAX_HEIGHT], int* width, int* height);
 void GetDimensions(int* width, int* height);
 void LiveCells(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height);
 int IsCellPosValid(int i, int j, int width, int height, int color);
 void PrintBoard(const board[MAX_WIDTH][MAX_HEIGHT], int width, int height);
+void GamePlay(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height, int maxGen);
+void ProcessBoard(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height);
+void AdvanceGen(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height);
+char AdjacentColor(char board[MAX_WIDTH][MAX_HEIGHT], int i, int j, int width, int height);
+
 
 
 
@@ -27,8 +32,8 @@ int main()
 	int width, height;
 	int maxGen;
 
-	GameStartup(board, &width, &height, &maxGen);
-
+	GameStart(board, &width, &height, &maxGen);
+	GamePlay(board, width, height, maxGen);
 
 
 	_getch();
@@ -36,10 +41,14 @@ int main()
 	return 0;
 }
 
-void GameStartup(char board[MAX_WIDTH][MAX_HEIGHT], int* width, int* height)
+void GameStart(char board[MAX_WIDTH][MAX_HEIGHT], int* width, int* height, int* maxGen)
 {
 	printf("Welcome to the game of life!\nSettings:\n");
 	InitializeBoard(board, width, height);
+	GetMaxGen(maxGen);
+	ProcessBoard(board, *width, *height);
+	printf("Welcome to the game of life!\nThis is the initial board :\n");
+	PrintBoard(board, *width, *height);
 
 }
 
@@ -62,8 +71,9 @@ void GetDimensions(int* width, int* height)
 void InitializeBoard(char board[MAX_WIDTH][MAX_HEIGHT], int* width, int* height)
 {
 	int i, j;
-	for (i = 0; i < MAX_WIDTH; i++) for (j = 0;j < MAX_HEIGHT;j++)
-		board[i][j] = '-';
+	for (i = 0; i < MAX_WIDTH; i++)
+		for (j = 0;j < MAX_HEIGHT;j++)
+			board[i][j] = '-';
 	GetDimensions(width, height);
 	LiveCells(board, *width ,*height);
 	PrintBoard(board, *width, *height);
@@ -86,7 +96,7 @@ void LiveCells(char board[MAX_WIDTH][MAX_HEIGHT] ,int width ,int height)
 		{
 			printf("Enter x y and color (R/G):\n");
 			scanf("%d %d %c", &i, &j, &color);
-			i--, j--;
+	//		i--, j--;
 			if (color == 'g')
 					color = 'G';
 			if (color == 'r')
@@ -96,7 +106,7 @@ void LiveCells(char board[MAX_WIDTH][MAX_HEIGHT] ,int width ,int height)
 		} while (accepted != 1);
 		board[i][j] = color;
 	}
-
+	printf("\n");
 	return;
 }
 
@@ -120,7 +130,7 @@ void PrintBoard(const char board[MAX_WIDTH][MAX_HEIGHT], int width, int height)
 			printf("%c", board[i][j]);
 		printf("\n");
 	}
-
+	printf("\n");
 	return;
 }
 
@@ -131,3 +141,100 @@ long int GetMaxGen(int* maxGen)
 
 }
 
+void GamePlay(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height, int maxGen)
+{
+	ProcessBoard(board, width, height);
+
+	return;
+}
+
+void ProcessBoard(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height)
+{
+	AdvanceGen(board, width, height);
+
+	return;
+}
+
+void AdvanceGen(char board[MAX_WIDTH][MAX_HEIGHT], int width, int height)
+{
+	int i, j, a;
+	char newBoard[MAX_WIDTH][MAX_HEIGHT];
+	for (i = 0; i < width; i++)
+		for (j = 0;j < height;j++)
+	{
+		a = AdjacentTo(board, i, j, width, height);
+		if (a == 2)
+			newBoard[i][j] = board[i][j];
+		if (a == 3)
+			if (board[i][j]=='-')
+				newBoard[i][j] = AdjacentColor(board, i, j, width, height);
+			else
+				newBoard[i][j] = board[i][j];
+		
+		if (a < 2 || a > 3)
+			newBoard[i][j] = '-';
+
+
+	}
+	for (i = 0; i < width; i++)
+		for (j = 0;j < height;j++)
+			board[i][j] = newBoard[i][j];
+
+
+
+	return;
+}
+
+int AdjacentTo(char board[MAX_WIDTH][MAX_HEIGHT], int i, int j, int width, int height)
+{
+	int k, l, count = 0;
+	for (k = -1 ; k <= 1 ; k++) 
+		for (l = -1 ; l <= 1 ; l++)
+	{
+		if (k != l)
+			if (board[XAdd(i,k,width)][YAdd(j,l,height)] != '-')
+				count++;
+	}
+	return count;
+}
+
+int XAdd(int i, int a, int width)
+{
+	i += a;
+	if (i < 0) 
+		i += width;
+	if (i >= width) 
+		i -= width;
+	return i;
+}
+
+int YAdd(int i, int a, int height)
+{
+	i += a;
+	if (i < 0)
+		i += height;
+	if (i >= height)
+		i -= height;
+	return i;
+}
+
+char AdjacentColor(char board[MAX_WIDTH][MAX_HEIGHT], int i, int j, int width, int height)
+{
+	int k, l, count = 0, color;
+	for (k = -1; k <= 1; k++)
+		for (l = -1; l <= 1; l++)
+		{
+		if (k != l)
+		{
+			if (board[XAdd(i, k, width)][YAdd(j, l, height)] = 'R')
+				count++;
+			if (board[XAdd(i, k, width)][YAdd(j, l, height)] = 'G')
+				count--;
+		}
+		
+	}
+	if (count == 1)
+		return 'R';
+	if (count == -1)
+		return 'G';
+}
